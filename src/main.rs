25aa4519f73a8cbaccae67f1664e84d48cc5a784
RWalling21 <rhw8246@rit.edu>
2024-06-02 21:33:41 -0400

    Implemented async i/o using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,37 @@
-use std::{
-    fs,
-    net::{TcpListener, TcpStream}, 
-    io::{prelude::*, BufReader},
-    thread,
-    time::Duration,
-};
-use rustweb::ThreadPool;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::time::{sleep, Duration};
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut request_line = String::new();
+    buf_reader.read_line(&mut request_line).await.unwrap();
 
-    let (status_line, filename) = match &request_line[..] {
+    let (status_line, filename) = match request_line.trim() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
+            sleep(Duration::from_secs(5)).await;
             ("HTTP/1.1 200 OK", "hello.html")
         }
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = tokio::fs::read_to_string(filename).await.unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(stream);
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_connection(stream).await;
         });
     }
-
-    println!("Shutting down.");
 }
